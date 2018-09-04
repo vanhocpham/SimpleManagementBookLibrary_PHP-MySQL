b7e0df8310f45dcfba79999c9e2b6abd585388af
hoc-anms <phamvanhoc96@gmail.com>
2018-09-04 13:45:27 +0700

    update function for categories dashboard

--- admin/categories-dashboard.php
+++ admin/categories-dashboard.php
@@ -12,12 +12,14 @@
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.3.1/css/all.css" integrity="sha384-mzrmE5qonljUremFsqc01SB46JvROS7bZs3IO2EmfFsd15uHvIt+Y8vEf7N7fWAU" crossorigin="anonymous">
     <script type="text/javascript" src="./js/categories-main.js"></script>
+    <link rel="stylesheet" href="./css/style.css">
 </headed>
 <body>
 <?php
 //Navar
 include_once './templates/categories-header.php'
 ?>
+
 <br>
 <div class="container" >
     <div class="row">
