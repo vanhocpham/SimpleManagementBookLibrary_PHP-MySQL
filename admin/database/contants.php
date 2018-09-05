cd713e2de7a29e1efcb91885449850282d79b065
hoc-anms <phamvanhoc96@gmail.com>
2018-09-05 14:50:18 +0700

    define host, user, pass in db

---
+++ admin/database/contants.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Hoc_Anms
+ * Date: 9/4/2018
+ * Time: 3:26 PM
+ */
+    define("HOST","localhost");
+    define("USER","root");
+    define("PASS","");
+    define("DB","mydb");
