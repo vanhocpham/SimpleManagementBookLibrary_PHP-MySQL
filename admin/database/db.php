93e2627d028db38ead5dde893b365b366bbf2a8a
hoc-anms <phamvanhoc96@gmail.com>
2018-09-05 14:49:01 +0700

    use pdo connect database

---
+++ admin/database/db.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Hoc_Anms
+ * Date: 9/4/2018
+ * Time: 3:29 PM
+ */
+class Database{
+    private $con;
+    public function connect(){
+        include_once 'contants.php';
+        $this->con=new Mysqli(HOST,USER,PASS,DB);
+        if ($this->con){
+            return $this->con;
+        }
+        return "DATABASE_CONNECTION_FAIL";
+    }
+}
