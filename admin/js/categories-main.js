5a2a22373e7424f0b6cd3252ee34e4981ffafc6b
hoc-anms <phamvanhoc96@gmail.com>
2018-09-06 16:39:20 +0700

    update function for categories-main js

--- admin/js/categories-main.js
+++ admin/js/categories-main.js
@@ -1,4 +1,22 @@
+$(document).ready(function(){
+    $("#register_form").on("submit",function(){
+        var status = false;
+        var name = $("#username");
+        var email = $("#email");
+        var pass1 = $("#password1");
+        var pass2 = $("#password2");
+        var type = $("#usertype");
+        //rizwan@gmail.com
+        var e_patt = new RegExp(/^[a-z0-9_-]+(\.[a-z0-9_-]+)*@[a-z0-9_-]+(\.[a-z0-9_-]+)*(\.[a-z]{2,4})$/);
+        if(name.val() == "" || name.val().length < 6){
+            name.addClass("border-danger");
+            $("#u_error").html("<span class='text-danger'>Please Enter Name and Name should be more than 6 char</span>");
+            status = false;
+        }else{
+            name.removeClass("border-danger");
+            $("#u_error").html("");
+            status = true;
+        }
+    })
 
-//$(document).ready(function () {
-   // alert("Hello My Friend!")
-//})
+  })
