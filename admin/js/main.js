1aeeed54f7b358b6e5a8a73b673f4ca083c561bf
hoc-anms <phamvanhoc96@gmail.com>
2018-09-07 17:24:16 +0700

    update function for main.js

--- admin/js/main.js
+++ admin/js/main.js
@@ -1,5 +1,6 @@
 
 $(document).ready(function(){
+    var DOMAIN="http://localhost:8088/Projects/BookLibrary/admin";
     $("#register_form").on("submit",function(){
         var status = false;
         var name = $("#username");
@@ -40,10 +41,10 @@ $(document).ready(function(){
             pass2.addClass("border-danger");
             $("#p2_error").html("<span class='text-danger'>Please Enter more than 8 digit password</span>");
             status = false;
-        }else{
+        }else {
             pass2.removeClass("border-danger");
             $("#p2_error").html("");
-            status = false;
+            status = true;
         }
         if(type.val()=="") {
             type.addClass("border-danger");
@@ -54,12 +55,89 @@ $(document).ready(function(){
             $("#t_error").html("");
             status=true;
         }
-        if(pass1.val()==pass2.val()){
-
+        if(pass1.val()==pass2.val()&&status==true){
+            $.ajax({
+                url:DOMAIN+"/includes/process.php",
+                method:"POST",
+                data:$("#register_form").serialize(),
+                success : function (data) {
+                    if (data=="EMAIL_ALREADY_EXISTS"){
+                        alert("This email is used");
+                    }
+                    else if(data=="SOME_ERROR"){
+                        alert("Something wrong!")
+                    }
+                    else {
+                        window.location.href=encodeURI(DOMAIN+"/categories-index.php?msg=You+are+register+success+Now+you+can+login");
+                    }
+                }
+            })
         }else {
             pass2.addClass("border-danger");
-            $("#p2_error").html("<span class='text-danger'>Please Enter more than 8 digit password</span>");
+            $("#p2_error").html("<span class='text-danger'>Password Not Matched</span>");
             status=false;
         }
     })
+
+
+    //for site login
+    $("#login_form").on("submit",function () {
+        var email=$("#log_email");
+        var pass=$("#log_pass");
+        var status=false;
+        var e_patt = new RegExp(/^[a-z0-9_-]+(\.[a-z0-9_-]+)*@[a-z0-9_-]+(\.[a-z0-9_-]+)*(\.[a-z]{2,4})$/);
+        if(email.val()==""){
+            email.addClass("border-danger");
+            $("#e_error").html("<span class='text-danger'>Please enter email<span>");
+            status=false;
+        }else if(!e_patt.test(email.val())){
+            email.addClass("border-danger");
+            $("#e_error").html("<span class='text-danger'>Email is valid.Please re-enter<span>");
+            status=false;
+        }else {
+            email.removeClass("border-danger");
+            $("#e_error").html("");
+            status=true;
+        }
+        if(pass.val()==""||pass.val().length<8){
+            pass.addClass("border-danger");
+            $("#p_error").html("<span class='text-danger'>Please enter password and password should more 8 digit</span>");
+            status=false;
+        }else {
+            pass.removeClass("border-danger");
+            $("#p_error").html("");
+            status=true;
+        }
+        if(status){
+            $.ajax({
+                url:DOMAIN+"/includes/process.php",
+                method:"POST",
+                data:$("#login_form").serialize(),
+                success : function (data) {
+                    if (data=="NOT_REGISTERD"){
+                        email.addClass("border-danger");
+                        $("#e_error").html("<span class='text-danger'>This email is not register, please new email</span>");
+                     }
+                    else if(data=="PASSWORD_NOT_MATCHED"){
+                        pass.addClass("border-danger");
+                        $("#p_error").html("<span class='text-danger'>Password not matched, please re-enter</span>")
+                        status=false;
+                    }
+                    else {
+                        console.log(data);
+                        window.location.href=encodeURI(DOMAIN+"/categories-dashboard.php?msg=Success+Login!+Welcome+to+dashboard");
+                    }
+                }
+            })
+        }
+    })
 })
+        //show password
+function showPass() {
+    var x = document.getElementById("log_pass");
+    if (x.type === "password") {
+        x.type = "text";
+    } else {
+        x.type = "password";
+    }
+}
