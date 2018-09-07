d276d0fadbbb7d4770fe4aae9806e04587b59975
hoc-anms <phamvanhoc96@gmail.com>
2018-09-07 17:20:36 +0700

    update function register for site

--- admin/categories-index.php
+++ admin/categories-index.php
@@ -11,8 +11,8 @@
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.3.1/css/all.css" integrity="sha384-mzrmE5qonljUremFsqc01SB46JvROS7bZs3IO2EmfFsd15uHvIt+Y8vEf7N7fWAU" crossorigin="anonymous">
-    <script type="text/javascript" src="./js/categories-main.js"></script>
     <link rel="stylesheet" href="./css/style.css">
+    <script type="text/javascript" src="./js/main.js"></script>
 </headed>
 <body>
     <?php
@@ -23,22 +23,34 @@
     ?>
     <br><br><br>
     <div class="container">
+        <?php
+            if (isset($_GET['msg'])AND !empty($_GET['msg'])){
+                ?>
+                <div class="alert alert-success alert-dismissible fade show" role="alert">
+                    <?php echo $_GET['msg'];?>
+                    <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+                        <span aria-hidden="true">&times;</span>
+                    </button>
+                </div>
+                <?php
+            }
+        ?>
         <div class="card mx-auto" style="width: 25rem;">
             <img class="card-img-top mx-auto" style="width: 40%;" src="./images/login.png" alt="Login Icon">
             <div class="card-body">
-                <h5 class="card-title"></h5>
-                <form>
+                <form id="login_form" onsubmit="return false">
                     <div class="form-group">
                         <label for="exampleInputEmail1">Email address</label>
-                        <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email">
-                        <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
+                        <input type="email" class="form-control" name="log_email" id="log_email"  placeholder="Enter email">
+                        <small id="e_error" class="form-text text-muted">We'll never share your email with anyone else.</small>
                     </div>
                     <div class="form-group">
                         <label for="exampleInputPassword1">Password</label>
-                        <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
+                        <input type="password" class="form-control" id="log_pass" name="log_pass" placeholder="Password">
+                        <small id="p_error" class="form-text text-muted"></small>
                     </div>
                     <div class="form-group form-check">
-                        <input type="checkbox" class="form-check-input" id="exampleCheck1">
+                        <input type="checkbox" class="form-check-input" id="exampleCheck1" onclick="showPass()">
                         <label class="form-check-label" for="exampleCheck1">Check me out</label>
                     </div>
                     <button type="submit" class="btn btn-primary"><i class="fa fa-sign-in-alt">&nbsp;</i>Login</button>
