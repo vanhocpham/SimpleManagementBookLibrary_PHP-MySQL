f84afc25b6974ae0edf2f9794666596feccadf2a
hoc-anms <phamvanhoc96@gmail.com>
2018-09-07 17:21:53 +0700

    update for database

--- admin/database/contants.php
+++ admin/database/contants.php
@@ -5,7 +5,11 @@
  * Date: 9/4/2018
  * Time: 3:26 PM
  */
+
+    session_start();
+
     define("HOST","localhost");
     define("USER","root");
     define("PASS","");
     define("DB","mydb");
+    define("DOMAIN","http://localhost:8088/Projects/BookLibrary/admin");
