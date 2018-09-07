8536ba2bb9f42d8cd278226f403c3569eb63ffe3
hoc-anms <phamvanhoc96@gmail.com>
2018-09-07 17:25:50 +0700

    update function for login

--- admin/js/categories-main.js
+++ admin/js/categories-main.js
@@ -1,3 +1,4 @@
+
 $(document).ready(function(){
     $("#register_form").on("submit",function(){
         var status = false;
@@ -6,7 +7,7 @@ $(document).ready(function(){
         var pass1 = $("#password1");
         var pass2 = $("#password2");
         var type = $("#usertype");
-        //rizwan@gmail.com
+
         var e_patt = new RegExp(/^[a-z0-9_-]+(\.[a-z0-9_-]+)*@[a-z0-9_-]+(\.[a-z0-9_-]+)*(\.[a-z]{2,4})$/);
         if(name.val() == "" || name.val().length < 6){
             name.addClass("border-danger");
@@ -17,6 +18,48 @@ $(document).ready(function(){
             $("#u_error").html("");
             status = true;
         }
-    })
+        if(!e_patt.test(email.val())){
+            email.addClass("border-danger");
+            $("#e_error").html("<span class='text-danger'>Please Enter Valid Email Address</span>");
+            status = false;
+        }else{
+            email.removeClass("border-danger");
+            $("#e_error").html("");
+            status = true;
+        }
+        if(pass1.val()==""||pass1.val().length<8){
+            pass1.addClass("border-danger");
+            $("#p1_error").html("<span class='text-danger'></span>");
+            status = false;
+        }else{
+            pass1.removeClass("border-danger");
+            $("#p1_error").html("");
+            status = true;
+        }
+        if(pass2.val()==""||pass2.val().length<8){
+            pass2.addClass("border-danger");
+            $("#p2_error").html("<span class='text-danger'></span>");
+            status = false;
+        }else{
+            pass2.removeClass("border-danger");
+            $("#p2_error").html("");
+            status = true;
+        }
+        if(type.val()=="") {
+            type.addClass("border-danger");
+            $("#t_error").html("<span class='text-danger'>Let's a usertype</span>");
+            status = false;
+        }else {
+            type.removeClass("border-danger");
+            $("#t_error").html("");
+            status=true;
+        }
+        if(pass1.val()==pass2.val()){
 
-  })
+        }else {
+            pass2.addClass("border-danger");
+            $("#p2_error").html("<span class='text-danger'>Password Not Matched</span>");
+            status=false;
+        }
+    })
+})
