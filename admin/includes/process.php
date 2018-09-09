7cbbe5ddbdd1903e1bc510b3d2bd42084d247dbf
hoc-anms <phamvanhoc96@gmail.com>
2018-09-09 22:43:36 +0700

    update function process

--- admin/includes/process.php
+++ admin/includes/process.php
@@ -6,11 +6,13 @@
  * Time: 1:25 PM
  */
     require '../database/contants.php';
+    require 'DBoperation.php';
     require 'user.php';
     if(isset($_POST["username"])AND isset($_POST["email"])){
         $user=new User();
         $result=$user->createUserAccount($_POST["username"],$_POST["email"],$_POST["password1"],$_POST["usertype"]);
         echo $result;
+        exit();
     }
 
     //for login process
@@ -18,4 +20,23 @@
         $user=new User();
         $result=$user->userLogin($_POST["log_email"],$_POST["log_pass"]);
         echo $result;
+        exit();
+    }
+
+
+    //to get categories
+    if(isset($_POST["getCategories"])){
+        $obj=new DBoperation();
+        $rows=$obj->getAllrecord("categories");
+        foreach ($rows as $row){
+            echo "<option value='".$row["cid"]."'>".$row["name_cat"]."</option>";
+        }
+        exit();
+    }
+    //add categories
+    if(isset($_POST["categories_name"]) AND isset($_POST["parent_cat"])){
+        $obj=new DBoperation();
+        $result=$obj->addCategories($_POST["parent_cat"],$_POST["categories_name"]);
+        echo $result;
+        exit();
     }
